147ac116b2b91a852d8e60317ba8992591fd19e1
Div_YT <comedhe83@gmail.com>
2023-07-16 20:16:24 -0600

    Finalisation of Project

--- nukamcvotingpage/src/App.js
+++ nukamcvotingpage/src/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import VotingCard from './components/shared/VotingCard/VotingCard'
+import voting from "./components/shared/VotingCard/voting.png";
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
@@ -12,7 +14,18 @@ class App extends Component {
           <h2>Welcome to NukaMC</h2>
         </div>
         <h1 className={"Voting-Intro"}>Thanks supporting NukaMC by Voting</h1>
-        <VotingCard/>
+
+        <div className={'Card-Header'} as={'h5'}>
+          Voting for NukaMC
+          <img className={'Title-Icon'} src={voting}></img>
+        </div>
+        <div className={'VotingCard'}>
+          <VotingCard title={'Best-Minecraft-Server.com'} description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi risus lacus, viverra vel justo nec, finibus pharetra odio. Etiam id turpis non sem blandit fermentum. Donec ultricies varius tellus, quis pulvinar tortor bibendum sit amet. Sed ante orci, congue eget lobortis a, scelerisque vel mi. Maecenas vestibulum elit sed mauris tristique, eget dapibus neque convallis. Donec molestie urna eget magna malesuada, eu consectetur turpis dignissim. Curabitur quam massa, imperdiet nec urna at, imperdiet pretium justo. Fusce velit orci, scelerisque in congue non, luctus eu nibh.'
+          } voteLink={"https://best-minecraft-servers.co/server-nukamc.17954/vote"} voteImage={"https://best-minecraft-servers.co/votebanner-17954-4D5988.png"}/>
+          <hr></hr>
+          <VotingCard title={"C'est Div"} description={'Comment Ca va?'} voteLink={"https://best-minecraft-servers.co/server-nukamc.17954/vote"} voteImage={"https://best-minecraft-servers.co/votebanner-17954-4D5988.png"}/>
+          <hr></hr>
+        </div>
       </div>
     );
   }
--- nukamcvotingpage/src/components/shared/VotingCard/VotingCard.js
+++ nukamcvotingpage/src/components/shared/VotingCard/VotingCard.js
@@ -4,24 +4,21 @@ import Card from 'react-bootstrap/Card';
 import Confetti from 'react-confetti';
 import voting from './voting.png';
 
-function VotingCard () {
+const VotingCard = (data) => {
+  const {title, description, voteLink, voteImage, props} = data
   const [isVoting, setIsVoting] = React.useState(false)
   return (
-    <div>
-      <div className={'Card-Header'} as={'h5'}>
-        Voting for NukaMC
-        <img className={'Title-Icon'} src={voting}></img>
-      </div>
+    <div property= {props}>
       <div className={"Card"} >
-        <h1 className={'Card-Title'}>Best-Minecraft-Server.com : </h1>
+        <h1 className={'Card-Title'}>{title}</h1>
         <h6 className={'Card-Text'}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi risus lacus, viverra vel justo nec, finibus pharetra odio. Etiam id turpis non sem blandit fermentum. Donec ultricies varius tellus, quis pulvinar tortor bibendum sit amet. Sed ante orci, congue eget lobortis a, scelerisque vel mi. Maecenas vestibulum elit sed mauris tristique, eget dapibus neque convallis. Donec molestie urna eget magna malesuada, eu consectetur turpis dignissim. Curabitur quam massa, imperdiet nec urna at, imperdiet pretium justo. Fusce velit orci, scelerisque in congue non, luctus eu nibh.
+          {description}
         </h6>
-        <a href={"https://best-minecraft-servers.co/server-nukamc.17954/vote"} target={'_blank'} onClick={() => {
+        <a href={voteLink} target={'_blank'} onClick={() => {
           setIsVoting(true)
         }}>
-          <img className={'Card-Image'} src={"https://best-minecraft-servers.co/votebanner-17954-4D5988.png"}></img>
-          <Confetti run={isVoting} recycle={!isVoting} numberOfPieces={300} onConfettiComplete={() => console.log('COMPLETED')}/>
+          <img className={'Card-Image'} src={voteImage}></img>
+          <Confetti run={isVoting} recycle={!isVoting} numberOfPieces={300}/>
         </a>
       </div>
     </div>
